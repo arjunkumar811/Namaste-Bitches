52a8d512accf8a4051d442a8942499df9421177c
arjunkumar811 <kumararjun26401@gmail.com>
2026-07-30 11:03:37 +0530

    Added Code

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'prisma/config';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
   migrations: {
     seed: 'npx tsx prisma/seed.ts',
   },
   datasource: {
-    url: process.env.DATABASE_URL || 'file:./dev.db',
+    url: process.env.DATABASE_URL,
   },
 });
